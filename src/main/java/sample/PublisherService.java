ad84b2305b1fa555a7d2490dcb2dce62b1651f56
Tushar V Shah <tshah@blizzard.com>
2018-11-28 11:04:59 -0800

    refactor: testing renaming

--- src/main/java/sample/PublisherService.java
+++ src/main/java/sample/PublisherService.java
@@ -4,9 +4,7 @@ public interface PublisherService {
     //
     // Name of the topic
     //
-    public static final String TOPICNAME = "tshah_test1";
-    //
-    // Publish Message API
-    //
+    String TOPICNAME = "tshah_test1";
+    /** This method publishes the message to given SNS Topic */
     void publish(String message, String topic) throws Exception;    
 }
